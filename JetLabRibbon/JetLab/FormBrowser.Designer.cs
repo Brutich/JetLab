9a44c65d8cc241cafd277854c7afc3dbba8f5962
Brutich <oleg87arh@gmail.com>
2018-11-22 01:28:25 +0300

    Run Script bottom

--- JetLabRibbon/JetLab/FormBrowser.Designer.cs
+++ JetLabRibbon/JetLab/FormBrowser.Designer.cs
@@ -30,6 +30,7 @@
 		{
             this.openFileDialog = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,14 +55,27 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonRunScript_Click);
+            // 
             // FormBrowser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.openFileDialog);
             this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
             this.Name = "FormBrowser";
             this.ShowIcon = false;
             this.Text = "Family Browser";
@@ -74,5 +88,6 @@
 
         private System.Windows.Forms.Button openFileDialog;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- JetLabRibbon/JetLab/FormBrowser.cs
+++ JetLabRibbon/JetLab/FormBrowser.cs
@@ -20,19 +20,26 @@ namespace JetLabRibbon.JetLab
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-
                 string fileName = openFileDialog1.FileName;
-
                 MessageBox.Show(fileName);
-
             }
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.ShowDialog();
+        }
 
+        private void buttonRunScript_Click(object sender, EventArgs e)
+        {
+            JetLabPlaceGroup.PlaceGroup placeGroupe = new JetLabPlaceGroup.PlaceGroup();
         }
     }
 }
