a528f5fccec4614d35bbc1da412aaad000922a47
Brutich <oleg87arh@gmail.com>
2018-12-17 23:38:34 +0300

    Copying existing room schedule

--- JetLabRibbon/JetLab/CreateSheets.cs
+++ JetLabRibbon/JetLab/CreateSheets.cs
@@ -29,11 +29,11 @@ namespace JetLabRibbon.JetLab
             // Find a titleblock in the project, or use InvalidElementId to create a sheet with no titleblock
             ElementId titleblockId = ElementId.InvalidElementId;
 
-            FilteredElementCollector collector = new FilteredElementCollector(document);
-            collector.OfClass(typeof(FamilySymbol));
-            collector.OfCategory(BuiltInCategory.OST_TitleBlocks).Where(q => q.Name == "0_TBL_NewForm3").First();
+            FilteredElementCollector tbCollector = new FilteredElementCollector(document);
+            tbCollector.OfClass(typeof(FamilySymbol));
+            tbCollector.OfCategory(BuiltInCategory.OST_TitleBlocks).Where(q => q.Name == "0_TBL_NewForm3").First();
 
-            FamilySymbol titleblock = collector.Cast<FamilySymbol>().FirstOrDefault();
+            FamilySymbol titleblock = tbCollector.Cast<FamilySymbol>().FirstOrDefault();
 
             if (titleblock != null)
                 titleblockId = titleblock.Id;
@@ -44,33 +44,11 @@ namespace JetLabRibbon.JetLab
             ElementId viewId = view.Id;
 
 
-            #region Point Location Calculation (WIP)
-           
-            // Get the BoundingBox of view.
-            BoundingBoxXYZ bbox = view.get_BoundingBox(view);
-            if (null == bbox)
-            {
-                throw new Exception("Selected view doesn't contain a bounding box.");
-            }
-           
-
-            // get the outline max and min,
-            // offset in each direction of max point of 
-            // bounding box than the outline box.
-
-            double dMaxOffset = 0.01;
-            XYZ ptMaxOutline = new XYZ(
-              view.Outline.Max.U,
-              view.Outline.Max.V, 0);
-
-            UV ptSourceViewOriginInSheet = new UV(
-              bbox.Max.X - dMaxOffset - ptMaxOutline.X,
-              bbox.Max.Y - dMaxOffset - ptMaxOutline.Y);
-
-            int scale = view.Scale;
-
-            XYZ point = ptMaxOutline;
-            #endregion
+            // Get explication schedule
+            FilteredElementCollector rsCollector = new FilteredElementCollector(document);
+            rsCollector.OfClass(typeof(ScheduleSheetInstance));
+            rsCollector.Where(q => q.Name == "50_ROM_L01_AS").First();
+            ScheduleSheetInstance existRoomScheduleInst = rsCollector.Cast<ScheduleSheetInstance>().FirstOrDefault();            
 
 
             #region Predefined Points
@@ -79,12 +57,6 @@ namespace JetLabRibbon.JetLab
                 -2.4925,
                  0.5036,
                  1.0819);
-
-            // Predefined point for schedule instance placement
-            XYZ pointScheduleInstance = new XYZ(
-                -1.5665,
-                 1.4124,
-                 0.0000);
 
             // Predefined point for legend 01 placement
             XYZ pointLeged_01 = new XYZ(
@@ -99,19 +71,34 @@ namespace JetLabRibbon.JetLab
                  0.0000);
             #endregion
 
-            // Begin to place view on sheet
+            // Begin to place views on sheet
             Transaction trans = new Transaction(document);
             trans.Start("Create Sheet and Place View");
 
             // Create the new sheet
             ViewSheet sheet = ViewSheet.Create(document, titleblockId);
             ElementId viewSheetId = sheet.Id;
+            if (null == viewSheetId)
+            {
+                throw new Exception("Create new sheet failed.");
+            }
 
             // Create the new viewport
-            Viewport viewport = Viewport.Create(document, viewSheetId, viewId, point);
+            Viewport viewport = Viewport.Create(document, viewSheetId, viewId, pointViewPort);
             if (null == viewport)
             {
                 throw new Exception("Place view failed.");
+            }
+
+            // Create the new room schedule
+            ScheduleSheetInstance newNoomScheduleInst = ScheduleSheetInstance.Create(
+                document,
+                viewSheetId,
+                existRoomScheduleInst.ScheduleId,
+                existRoomScheduleInst.Point);
+            if (null == newNoomScheduleInst)
+            {
+                throw new Exception("Place room schedule failed.");
             }
 
             trans.Commit();
