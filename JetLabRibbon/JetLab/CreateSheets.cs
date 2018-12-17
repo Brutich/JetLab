3a100da7b377cac8be9d501aaa73eff158789c93
brutich <Oleg87arh@gmail.com>
2018-12-17 18:41:03 +0300

    Predefined points

--- JetLabRibbon/JetLab/CreateSheets.cs
+++ JetLabRibbon/JetLab/CreateSheets.cs
@@ -38,18 +38,22 @@ namespace JetLabRibbon.JetLab
             if (titleblock != null)
                 titleblockId = titleblock.Id;
 
+
+            // Get plan view
             View view = document.ActiveView;
             ElementId viewId = view.Id;
 
+
+            #region Point Location Calculation (WIP)
+           
             // Get the BoundingBox of view.
             BoundingBoxXYZ bbox = view.get_BoundingBox(view);
             if (null == bbox)
             {
                 throw new Exception("Selected view doesn't contain a bounding box.");
             }
+           
 
-
-            #region Point Location Calculation (WIP)
             // get the outline max and min,
             // offset in each direction of max point of 
             // bounding box than the outline box.
@@ -69,11 +73,31 @@ namespace JetLabRibbon.JetLab
             #endregion
 
 
-            // Predefined point
-            point = new XYZ(
+            #region Predefined Points
+            // Predefined point for plan vieport placement
+            XYZ pointViewPort = new XYZ(
                 -2.4925,
-                0.5036,
-                1.0819);
+                 0.5036,
+                 1.0819);
+
+            // Predefined point for schedule instance placement
+            XYZ pointScheduleInstance = new XYZ(
+                -1.5665,
+                 1.4124,
+                 0.0000);
+
+            // Predefined point for legend 01 placement
+            XYZ pointLeged_01 = new XYZ(
+                -0.9366,
+                 0.5089,
+                 0.0000);
+
+            // Predefined point for legend 02 placement
+            XYZ pointLeged_02 = new XYZ(
+                -0.9504,
+                 0.3157,
+                 0.0000);
+            #endregion
 
             // Begin to place view on sheet
             Transaction trans = new Transaction(document);
